0e68b939d6c27eeb1b408624d71d1ed81ce628b0
aydin demircioglu <hello@aydindemircioglu.de>
2014-10-30 21:37:37 +0100

    attempt to support vs2012

--- README.txt
+++ README.txt
@@ -5,18 +5,19 @@ miniShark
 This is a little script that will do the following things
 	-Download, patch and compile boost 1.55
 	-Download and compile Shark from SVN (in release mode)
-	-Open VS2013 with an ready-to-compile example project 
+	-Open Visual Studio with an ready-to-compile example project 
 		from the tutorials
 
 For this, the package contains executables of cmake and svn
 (please turn on your virus-scanner, I do not guarantee anything), 
 
-The script currently needs Visual Studio 2013, if there is
-demand, a small adaptation to older Visual Studio versions is
-possible.
+The script currently needs Visual Studio 2013 or Visual Studio
+2012 with .NET Framework v4.0.30319 (It will search for 
+MSBuild.exe). If there is demand, a small adaptation to older 
+Visual Studio versions might be possible.
 
 Shark will be build in release mode and in debug mode, but
-the example is release only (so press ctrl+f5 in visual Studio 2013 
+the example is release only (so press ctrl+f5 in Visual Studio  
 to get the example solution running).
 
 In order to use this in your own projects, you basically have to
@@ -30,6 +31,15 @@ by starting cmake by hand, or reextracting the archive and
 rerunning the script. 
 
 
+Prerequisites
+=============
+
+-Visual Studio 2013 OR
+-Visual Studio 2012+.NET Framework v4.0.30319
+-Internet connection
+-5 GB of hard disk space
+
+
 Usage
 =====
 
@@ -38,7 +48,7 @@ Usage
 	https://github.com/sharkMLreports/miniShark)
 2. Double-click the 'miniShark.bat' file.
 3. Wait for a long time (45-60+ minutes)
-4. When everything is finished, Visual Studio 2013
+4. When everything is finished, Visual Studio 
 	will have been opened with an example tutorial solution.
 5. Press CTRL+F5 to compile.
 
--- minishark.bat
+++ minishark.bat
@@ -6,19 +6,37 @@ echo Portable Shark Windows v0.01
 echo ////////////////////////////
 echo.
 echo This will bootstrap a 'portable' boost version.
-echo Please make sure that you have at LEAST 3 GB space on your hard drive.
+echo Please make sure that you have at LEAST 5 GB space on your hard drive.
 echo You need to all this script only once. 
 echo After that build only the example solution.
 echo.
 pause 
 
+
+:TESTVS2013
 :: check the visual studio version, for now we only compile with vs2013
 reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.12.0" >> nul 2>&1
-if %ERRORLEVEL% NEQ 0 (echo VS 2013 is not installed. Currently VS 2013 is required.
-goto EXIT) 
+if %ERRORLEVEL% NEQ 0 (echo VS 2013 is not installed.
+goto TESTVS2012) 
 
 echo Found Visual Studio 2013. 
 echo.
+goto STARTVS2013
+
+
+:TESTVS2012
+reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.11.0" >> nul 2>&1
+if %ERRORLEVEL% NEQ 0 (echo VS 2012 is not installed.
+goto EXITNOTHINGFOUND) 
+
+
+echo Found Visual Studio 2012. 
+goto STARTVS2012
+
+
+
+
+::STARTVS2013
 
 :: obtain boost from internet. use 1.55
 tools\SlikSvn\bin\svn.exe co https://svn.boost.org/svn/boost/tags/release/Boost_1_55_0 boost
@@ -54,5 +72,65 @@ cd ..
 
 :: start visual studio 2013 with our example project
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" .\example\CSvmTutorial.sln
+
+goto EXIT
+
+
+
+
+:STARTVS2012
+
+IF EXIST C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe (
+    echo Found .NET Framework v4.0.3019.
+) else (
+    echo Unable to find .NET Framework v4.0.3019. Please install, or fix the batch file.
+    goto EXIT
+)
+
+
+:: obtain boost from internet. use 1.55
+tools\SlikSvn\bin\svn.exe co https://svn.boost.org/svn/boost/tags/release/Boost_1_55_0 boost
+
+:: but that needs patching. we have that patch here
+copy /Y patch\visualc.hpp boost\boost\config\compiler\visualc.hpp
+copy /Y patch\no_decltype_n3276_fail.cpp boost\libs\config\test\no_decltype_n3276_fail.cpp
+copy /Y patch\no_decltype_n3276_pass.cpp boost\libs\config\test\no_decltype_n3276_pass.cpp
+copy /Y patch\basic_text_iprimitive.cpp boost\libs\serialization\src\basic_text_iprimitive.cpp
+copy /Y patch\basic_text_oprimitive.cpp boost\libs\serialization\src\basic_text_oprimitive.cpp
+copy /Y patch\basic_text_wiprimitive.cpp boost\libs\serialization\src\basic_text_wiprimitive.cpp
+copy /Y patch\basic_text_woprimitive.cpp boost\libs\serialization\src\basic_text_woprimitive.cpp
+copy /Y patch\has_member_function_callable_with.hpp boost\boost\intrusive\detail\has_member_function_callable_with.hpp 
+
+:: obtain shark from svn
+tools\SlikSvn\bin\svn.exe co https://svn.code.sf.net/p/shark-project/code/trunk/Shark shark
+
+:BOOST2012
+:: generate boost
+cd boost
+CALL bootstrap.bat
+b2
+cd ..
+
+:SHARK2012
+:: generate visual studio solution
+cd shark
+..\tools\cmake\bin\cmake.exe -G "Visual Studio 11 2012" -DBOOST_INCLUDEDIR=..\boost -DBOOST_LIBRARYDIR=..\boost\stage\lib -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="/EHsc"
+
+:: compile shark
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" shark.sln /p:Configuration=Release
+cd ..
+
+:: start visual studio 2012 with our example project
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" .\example\CSvmTutorial_vs2012.sln
+
+goto EXIT
+
+
+
+:EXITNOTHINGFOUND
+echo No proper version of Visual Studio found. Exiting.
+echo.
+pause
+
 
 :EXIT
